5b3b2c865f42e94d042fb4ccf9fe8d0f28702411
trrstnn <edeclarin@gmail.com>
2019-04-02 19:35:08 -0700

    creates register and login form

---
+++ forms.py
@@ -0,0 +1,68 @@
+from flask_wtf import FlaskForm as Form
+
+from wtforms import TextField, TextAreaField, SubmitField, StringField, PasswordField, IntegerField, BooleanField
+from wtforms import SelectField
+
+from wtforms.validators import (DataRequired, Regexp, ValidationError, Email, Length, EqualTo, NumberRange)
+
+from flask_login import UserMixin
+
+from flask_bcrypt import generate_password_hash
+
+from models import User
+
+def name_exists(form, field):
+    if User.select().where(User.username == field.data). exists():
+        raise ValidationError("User with this username already exists!")
+
+def email_exists(form, field):
+    if User.select().where(User.email == field.data).exists():
+        raise ValidationError("Someone with this email is already in the DB")
+
+
+
+
+
+class RegisterForm(Form):
+    username = StringField(
+        'Username',
+        validators=[
+            DataRequired(),
+            Regexp(
+                r'^[a-zA-Z0-9_]+$',
+                message=("Username should be one word, letters, numbers, and underscores only")
+            ),
+            name_exists
+        ])
+    email = StringField(
+        'Email',
+        validators = [
+            DataRequired(),
+            Email(),
+            email_exists
+        ])
+    password = PasswordField(
+        'Password',
+        validators = [
+            DataRequired(),
+            Length(min=2),
+            EqualTo('Password2', message='Passwords must match')
+        ])
+    Password2 = PasswordField(
+        'Confirm Password',
+        validators = [DataRequired()]
+    )
+    name = StringField(
+        'Name',
+        validators = [
+            DataRequired()
+        ])
+    freelancer = BooleanField(
+        'Are you a freelancer?',
+        validators = [
+            DataRequired()
+        ])
+  
+class LoginForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Email() ])
+    password = PasswordField('Password', validators=[DataRequired() ])
